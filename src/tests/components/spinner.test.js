05819060585f0847ccdfcd3bcbf5d00c44446fd2
delvallejonatan <delvallejonatan@gmail.com>
2017-06-16 01:55:41 +0200

    Update test

--- src/tests/components/spinner.test.js
+++ src/tests/components/spinner.test.js
@@ -18,18 +18,30 @@ describe('test <Spinner /> component', () => {
     expect(toJSON(wrapper)).toMatchSnapshot()
   })
 
-  it('it set the height to 50', () => {
+  it('it set the height and width to 50', () => {
     const wrapper = render(
       <Spinner
         loading={true}
         height={50}
       />
     )
-
     const Ring = wrapper.find('img')
 
+    expect(Ring.length).toBe(1)
     expect(Ring['0'].attribs.height).toBe('50')
     expect(Ring['0'].attribs.width).toBe('50')
+    expect(toJSON(wrapper)).toMatchSnapshot()
+  })
+
+  it('not renders if loading is true', () => {
+    const wrapper = render(
+      <Spinner
+        loading={false}
+      />
+    )
+    const Ring = wrapper.find('img')
+
+    expect(Ring.length).toBe(0)
     expect(toJSON(wrapper)).toMatchSnapshot()
   })
 
